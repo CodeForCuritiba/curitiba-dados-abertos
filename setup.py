cd06da24ed1f405a89e1c33510efdd5bd559fcaf
Jonhnatha Trigueiro <joepreludian@gmail.com>
2020-02-10 02:41:18 -0300

    Update workflow

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 
 setup(
     name='curitiba-dados-abertos',
-    version='0.0.1',
+    version='1.0.0',
     description='API Used to ease the access to the datasets of Curitiba Dados Abertos portal',
     author='Jonhnatha Trigueiro',
     author_email='joepreludian@gmail.com',
     url='https://github.com/CodeForCuritiba/curitiba-dados-abertos',
     packages=['curitiba_dados_abertos'],
     license='MIT License',
-    requires=[
+    install_requires=[
         'requests (>2.20)'
     ],
     long_description=open('README.md').read()
