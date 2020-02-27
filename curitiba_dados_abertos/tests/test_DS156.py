401ebc0a0671fde698297a7c0791668caa716f05
Jonhnatha Trigueiro <joepreludian@gmail.com>
2020-02-26 22:40:38 -0300

    Update tests

--- curitiba_dados_abertos/tests/test_DS156.py
+++ curitiba_dados_abertos/tests/test_DS156.py
@@ -45,3 +45,26 @@ def test_ds_get_available_list(ds156):
 
     for item in list_items:
         assert bool(re.match(r'(2[0-9]{3}-[0-9]{2}-[0-9]{2})', item)) == True
+
+def test_ds_get_pandas_dataframe_latest(ds156):
+    pd_ds156 = ds156.get_pandas_dataframe()
+
+    assert pd_ds156 is not None
+    assert type(pd_ds156['ASSUNTO'].to_list()) == list
+
+def test_ds_get_pandas_dataframe_clean_data(ds156):
+    pd_ds156 = ds156.get_pandas_dataframe(apply_cleanup=False)
+
+    clean_data = ds156.clean_data(pd_ds156)
+
+    assert clean_data is not None
+    assert 'pandas.core.frame.DataFrame' in str(type(clean_data))
+
+def test_ds_get_pandas_dataframe_dated(ds156):
+    list_items = ds156.list_available_items()
+    date_prefix_item = list_items[0]
+
+    pd_ds156 = ds156.get_pandas_dataframe(date_prefix=date_prefix_item)
+
+    assert pd_ds156 is not None
+    assert type(pd_ds156['ASSUNTO'].to_list()) == list
